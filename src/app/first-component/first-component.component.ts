3a2977a10ae16ca811c668819f57cd1c592c6564
silascaimi <silascaimi@live.com>
2021-08-07 13:20:23 -0300

    criando um componente

---
+++ src/app/first-component/first-component.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'first-component',
+  templateUrl: './first-component.html'
+})
+export class FirstComponent {
+  title = 'Primeiro componente';
+}
