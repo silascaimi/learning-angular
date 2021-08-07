13839625bc15b03fecbe2277d3255060622986a3
silascaimi <silascaimi@live.com>
2021-08-07 13:24:26 -0300

    desconsiderar

--- src/app/first-component/first-component.component.ts
+++ src/app/first-component/first-component.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'first-component',
-  templateUrl: './first-component.html'
+  templateUrl: './first-component.component.html'
 })
 export class FirstComponent {
   title = 'Primeiro componente';
