203233e2b98320c459b46d1d8015987b1dc4bbe9
silascaimi <silascaimi@live.com>
2021-08-07 14:04:25 -0300

    Property Biding

--- src/app/first-component/first-component.component.ts
+++ src/app/first-component/first-component.component.ts
@@ -7,4 +7,6 @@ import { Component } from "@angular/core";
 export class FirstComponent {
   title = 'Primeiro componente';
   elements = ['Primeiro', 'Segundo', 'Terceiro'];
+  imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/250px-Angular_full_color_logo.svg.png';
+  imageWidth = 200;
 }
