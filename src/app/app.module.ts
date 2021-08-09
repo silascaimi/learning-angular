e90edc6b446fddcd1ae125ba1639dce29d46d484
silascaimi <silascaimi@live.com>
2021-08-09 20:29:32 -0300

    usando Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,26 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first-component/first-component.component';
 import { ConvertToSpacesPipe } from './resources/convert-to-spaces';
-import { SecondComponentComponent } from './second-page/second-component/second-component.component';
+import { SecondComponent } from './second-page/second-component/second-component.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
     ConvertToSpacesPipe,
-    SecondComponentComponent
+    SecondComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'first-component', component: FirstComponent },
+      { path: 'second-component', component: SecondComponent },
+      { path: '', redirectTo: 'first-component', pathMatch: 'full' },
+      { path: '**', redirectTo: 'first-component', pathMatch: 'full' },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/second-page/second-component/second-component.component.ts
+++ src/app/second-page/second-component/second-component.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './second-component.component.html',
   styleUrls: ['./second-component.component.css']
 })
-export class SecondComponentComponent implements OnInit {
+export class SecondComponent implements OnInit {
 
   constructor() { }
 
