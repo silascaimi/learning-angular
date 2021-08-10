6b563e3a0804553d40f5bbae7db7855d053d7072
silascaimi <silascaimi@live.com>
2021-08-09 23:15:03 -0300

    Route com passagem de parâmetro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { FirstComponent } from './first-component/first-component.component';
 import { ConvertToSpacesPipe } from './resources/convert-to-spaces';
 import { SecondComponent } from './second-page/second-component/second-component.component';
 import { RouterModule } from '@angular/router';
+import { ThirdComponent } from './third-page/third/third.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
     ConvertToSpacesPipe,
-    SecondComponent
+    SecondComponent,
+    ThirdComponent
   ],
   imports: [
     BrowserModule,
@@ -22,6 +24,7 @@ import { RouterModule } from '@angular/router';
     HttpClientModule,
     RouterModule.forRoot([
       { path: 'first-component', component: FirstComponent },
+      { path: 'first-component/:id', component: ThirdComponent },
       { path: 'second-component', component: SecondComponent },
       { path: '', redirectTo: 'first-component', pathMatch: 'full' },
       { path: '**', redirectTo: 'first-component', pathMatch: 'full' },
--- src/app/third-page/third/third.component.ts
+++ src/app/third-page/third/third.component.ts
@@ -1,4 +1,6 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'lng-third',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThirdComponent implements OnInit {
 
-  constructor() { }
+  pageTitle: string = 'Terceiro componente';
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle += `: Produto ${id}`;
   }
 
 }
